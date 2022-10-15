45c496f222e3c5ca92ceb22d0d162b07af481ba7
hwangblood <hwangblood@qq.com>
2022-10-15 23:37:58 +0800

    把相关的资源放在一起（逻辑嵌套）

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+const v1MemberRouter = require("./v1/routes/memberRoutes");
 
-const app = express(); 
-const PORT = process.env.PORT || 3000; 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 app.use("/api/v1/workouts", v1WorkoutRouter);
+app.use("/api/v1/members", v1MemberRouter);
 
-app.listen(PORT, () => { 
-    console.log(`API is listening on port ${PORT}`); 
+app.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`);
 });
